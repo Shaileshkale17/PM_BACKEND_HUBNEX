d7ca9d7a891cd109150c53e75939467dc9d59502
Shailesh kale <shaileshkale87730@gmail.com>
2024-06-18 19:43:38 +0530

    remoing the middleware

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ import AssignLeadrouter from "./router/AssignLead.routes.js";
 import clientRoutes from "./router/clientRoutes.routes.js";
 import clientinfoRoutes from "./router/clients.js";
 import FromDataRoutes from "./router/FromData.routes.js";
+import {
+  FetchAllData,
+  UpdateData,
+} from "./controllers/AssignLead.controllers.js";
 const app = express();
 app.use(
   cors({
@@ -20,9 +24,11 @@ app.use(express.static("public"));
 app.use(cookieParser());
 
 // Routes Declarations
-app.use("/api/v1/", AssignLeadrouter);
-app.use("/api/v1/", clientRoutes);
-app.use("/api/v1/", clientinfoRoutes);
-app.use("/api/v1/", FromDataRoutes);
+//! AssignLead
+app.put("/api/v1/update/:id", UpdateData);
+app.get("/api/v1/fetch-all", FetchAllData);
+app.use("/api/v1/clients", clientRoutes);
+app.use("/api/v1/clientsinfo", clientinfoRoutes);
+app.use("/api/v1/FromData", FromDataRoutes);
 
 export { app };
